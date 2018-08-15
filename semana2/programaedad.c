dad4751c99230a692a908d263574ffed6b6779d1
diegoruizesparza6510 <escolares.ruizesparza@gmail.com>
2018-08-15 09:31:48 -0500

    programa edad semana 2

---
+++ semana2/programaedad.c
@@ -0,0 +1,15 @@
+/*creado el 15 de agosto del 2018 por Diego Ruiz Esparza*/
+#include<stdio.h>
+int main()
+{
+int edad,a;
+printf("hola Diego\n");
+printf("este es el 1er programa del curso\n");
+printf("introduce tu edad\n");
+scanf("%i",&edad);
+a=edad+10;
+printf("tu edad es:%i\n",a);
+return 0;
+}
+
+
