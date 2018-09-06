868fba2c989dc6495af557f66717330a4b3afac8
diegoruizesparza6510 <escolares.ruizesparza@gmail.com>
2018-09-06 08:36:06 -0500

    Numeros primos y funciones, falta binarios

--- semana5/evaluafunciones.c
+++ semana5/evaluafunciones.c
@@ -1,32 +1,33 @@
 #include<stdio.h>
 #include<math.h>
+
 int main()
 {
-int n,f,i,o;
-float s;
-float ex,lo,si,co,ra;
- while(o==1||2)
-{ if(o==2)
-  return 1;
- printf("Hola, este programa calcula el exponencial, logaritmo, seno, coseno y raíz cuadrada de una variable x \n");
- printf("¿Qué valor le daras a la variable x? \n");
- scanf("%f",&s);
- printf("¿En qué valor quieres que inicie el programa? \n");
- scanf("%i",&n);
- printf("¿En qué valor quieres que termine el programa? \n");
- scanf("%i",&f);
-for(i=n;i<f;i++)
-  { 
-    ex=exp(s);
-    lo=log(s);
-    si=sin(s);
-    co=cos(s);
-    ra=sqrt(s);
-    printf("%f %f %f %f %f \n",ex,lo,si,co,ra);
-  }
- printf("¿Quieres continuar el programa o salir? \n");
- printf("Presiona 1 para continuar o 2 para salir\n");
- scanf("%i",&o);
+int n,i;
+float x,del, ex, lo, si, co, ra, a, b;
+        printf("Este programa calcula el logaritmo,seno,coseno, raíz cuadrada y exponente de un numero :\n");
+	printf("Introduce el limite inferior:\n");
+	scanf("%f", &a);
+	printf("Introduce el limite superior: \n");
+	scanf("%f", &b);
+	printf("Introduce el espaciado que quieres:\n");
+	scanf("%f", &del);
+n=((b-a)/del);
+for(i=0;i<=n;i++)
+{
+
+     x=(a+(del*i));
+     ex=exp(x);
+     lo=log(x);
+     si=sin(x);
+     co=cos(x);
+     ra=sqrt(x);
+     printf("El número dado es %f \n", x); 
+     printf("El exponente del número es: %f \n", ex);
+     printf("El logaritmo del  número es:%f \n", lo);
+     printf("El seno del número es: %f \n", si);
+     printf("El coseno del número es:%f \n", co);
+     printf("La raíz del número es: %f \n", ra);
 }
 return 0;
 }
