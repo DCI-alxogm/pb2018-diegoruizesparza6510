7885452847f096048fc287ff75ccb7a91b7a8a5c
Ruiz Esparza Flores Diego <ruizesparzafd2018@dci25-163.ugto.mx>
2018-09-28 12:08:58 -0500

    oo#

--- Projecto1/Project1.c
+++ Projecto1/Project1.c
@@ -3,34 +3,31 @@
 
 int main(){
 //declaracion de variables
-float G,h,masap,Pi;
-int n,i,o;
-double a,b,c,d,e,f;
+float masap,Pi;
+int n=366,h,i,o;
+double G,a,b,c,d,e,f;
 double x[n],y[n],z[n],vx[n],vy[n],vz[n],r;
 FILE *planeta;
 FILE *resultados;
-G=4*(pow(Pi,2));
-n=1001;
-Pi=3.1416;
-h=0.001;
-masap=0.000003;
+G=52259512.185;
 
 //presentacion del programa
 printf("Hola, este programa calcula la orbita de diferentes planetas entorno al sol\n");
 	planeta=fopen("tierra.txt","r"); //dependiendo de la variable seran los datos(en este caso se van a leer los datos de mercurio)
-	fscanf(planeta,"%lf %lf %lf %lf %lf %lf",&a,&b,&c,&d,&e,&f);//leer los datos
-	printf("Prueba datos iniciales %lf\t %lf\t %lf\t %lf\t %lf\t %lf\n",a,b,c,d,e,f);//se guardan los datos en las variable
+	fscanf(planeta,"%lf %lf %lf %lf %lf %lf %f %i",&a,&b,&c,&d,&e,&f,&masap,&h);//leer los datos
+	printf("Prueba datos iniciales %lf\t %lf\t %lf\t %lf\t %lf\t %lf\t %f\t %i\t \n",a,b,c,d,e,f,masap,h);//se guardan los datos en las variable
 	fclose(planeta);
-	r=sqrt(pow(a,2)+pow(b,2)+pow(c,2));//se calcula el radio con los valores de x,y,z de cada planeta, en este caso los de mercurio
-	x[0]=a; //se asignan los valores a la ṕosición 0 de nuestros arreglos
+	//se asignan los valores a la ṕosición 0 de nuestros arreglos
+	x[0]=a; 
 	y[0]=b;
 	z[0]=c;
 	vx[0]=d;
 	vy[0]=e;
 	vz[0]=f;
 	resultados=fopen("resultadostierra.txt","w"); //se cierra el archivo de datos del planeta
-		for(i=1;i<1000;i++) //se van a hacer 1000 calculos ya que nuestro paso es de 0.001 y el valor de nuestro año es 1
+		for(i=1;i<366;i++) //se van a hacer 1000 calculos ya que nuestro paso es de 0.001 y el valor de nuestro año es 1
 	{
+	    r=sqrt(pow(x[i-1],2)+pow(y[i-1],2)+pow(z[i-1],2));//se calcula el radio con los valores de x,y,z de cada planeta, en este caso los de mercurio
 	    //se realizan operaciones, se pone i-1 porque tenemos que empezar con los valores iniciados dados en nuestros archivos de texto de los planetas
 	    x[i]=x[i-1]+vx[i-1]*h; 
 	    y[i]=y[i-1]+vy[i-1]*h;
