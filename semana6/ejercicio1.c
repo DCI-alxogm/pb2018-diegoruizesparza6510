5a67ff0d179371cd2c88d10f0c676d16bfac08b9
diegoruizesparza6510 <escolares.ruizesparza@gmail.com>
2018-10-03 09:19:54 -0500

    ll#

--- semana6/ejercicio1.c
+++ semana6/ejercicio1.c
@@ -1,73 +1,39 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<math.h>
+
 int main()
 {
-int N,i;
-char a,t=0;
-float temp,edad[N],sexo[N],sem[N],cali[N],semestre[9],h=0,m=0,p=0,edadsem[9],promedad=0,c[9];
-printf("Introduce el número de alumnos:\n");
-scanf("%i", &N);
-for(int i=0 ; i<9 ; i++)
+ FILE *fp;
+ int n,i;
+ float x,del, ex, lo, si, co, ra, a, b;
+ fp=fopen("funcion.txt","r");
+        printf("Este programa calcula el logaritmo,seno,coseno, raíz cuadrada y exponente de un numero :\n");
+	printf("Introduce el limite inferior:\n");
+	scanf("%f", &a);
+	printf("Introduce el limite superior: \n");
+	scanf("%f", &b);
+	printf("Introduce el espaciado que quieres:\n");
+	scanf("%f", &del);
+ n=((b-a)/del);
+ for(i=0;i<=n;i++)
 {
-semestre[i]=0;
-edadsem[i]=0;
-c[i]=0;
+
+     x=(a+(del*i));
+     ex=exp(x);
+     lo=log(x);
+     si=sin(x);
+     co=cos(x);
+     ra=sqrt(x);
+     printf("El número dado es %f \n", x); 
+     printf("El exponente del número es: %f \n", ex);
+     printf("El logaritmo del  número es:%f \n", lo);
+     printf("El seno del número es: %f \n", si);
+     printf("El coseno del número es:%f \n", co);
+     printf("La raíz del número es: %f \n", ra);
+     fscanf(fp,"%f %f %f %f %f %f",&x,&ex,&lo,&si,&co,&ra);
+
+
 }
-printf("A continuación, introduzca %i lineas de datos siguiendo:\n", N);
-printf("Edad,Sexo(H-M),Semestre,promedio\n");
-for(int i=0 ; i<N ; i++){
-scanf("%f %c %f %f", &edad[i], &a, &sem[i], &cali[i]);
-if(a=='h')
-{
-sexo[i]=0;
-h++;
-}
-else
-{
-sexo[i]=1;
-m++;
-}
-t = sem[i]-1;
-edadsem[t]+=edad[i];
-promedad+=edad[i];
-c[t]+=cali[i];
-p+=cali[i];
-semestre[t]++;
-}
-printf("Total de estudiantes: %i , De los cuales %0.0f son mujeres y %0.0f son hombres\n", N, m, h);
-printf("Estudiantes por semestre:\n");
-for(int i=0 ; i<8 ; i++)
-{
-printf("Semestre %i°    %0.0f \n", i+1, semestre[i]);
-}
-temp = p/N;
-printf("Promedio de calificaciones: %0.2f \n", temp);
-printf("Promedio de calificaciones por semestre:\n");
-for(int i=0 ; i<8 ; i++)
-{
-if(semestre[i]!=0)
-{
-temp= c[i]/semestre[i];
-}
-else
-{
-temp=0;
-}
-printf("\n Semestre %i°  %0.2f", i+1, temp);
-}
-temp = promedad/N;
-printf("\n Promedio de edad: %0.2f \n", temp);
-printf("\n Promedio de edad por semestre: \n");
-for(int i=0 ; i<8 ; i++)
-{
-if(semestre[i]!=0)
-{
-temp = edadsem[i]/semestre[i];
-}
-else
-{
-temp=0;
-}
-printf("Semestre %i°    %0.2f \n", i+1, temp);
-}
-return 0;
+ fclose(fp);
+ return 0;
 }
--- semana6/file.c
+++ semana6/file.c
@@ -5,12 +5,19 @@ FILE *archivo;
 float var1,var2;
 char var[255];
 archivo=fopen("test.txt","w");
-fputs("Esta es una prueba de fputs..\n",archivo);
+fputs("aqui se coloca el texto para que se imprima \n",archivo);
 fprintf(archivo,"fprint...\n");
-var1=0.15;
-var2=100.8;
+var1=0.155555;
+var2=100.8555;
 fprintf(archivo, "%f %f\n",var1,var2);
 fclose(archivo);
-
+archivo=fopen("test.txt","r");
+fgets(var,255,(FILE*)archivo);
+printf("%s\n",var);
+fscanf(archivo,"%s\n",var);
+printf("%s\n",var);
+fscanf(archivo,"%f %f",&var1,&var2);
+printf("%f %f \n",var1,var2);
+fclose(archivo);
 return 0;
 }
